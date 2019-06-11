3c5ab531f683e1c69236ad64b7c2c707461f7ba6
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-06-11 16:08:50 -0700

    first commit

---
+++ src/main/java/com/mahoney/code401d4/day12/introductionString/IntroductionStringApplication.java
@@ -0,0 +1,13 @@
+package com.mahoney.code401d4.day12.introductionString;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionStringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionStringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahoney/code401d4/day12/introductionString/IntroductionStringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mahoney.code401d4.day12.introductionString;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntroductionStringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
